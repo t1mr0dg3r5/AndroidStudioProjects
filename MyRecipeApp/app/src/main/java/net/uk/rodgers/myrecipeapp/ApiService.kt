80d30dda09487378cd003c741953552902c805e4
t1mr0dg3r5 <tim@beatsworkin.co.uk>
2026-07-11 15:10:34 +0100

    Working recipe categories api

--- MyRecipeApp/app/src/main/java/net/uk/rodgers/myrecipeapp/ApiService.kt
+++ MyRecipeApp/app/src/main/java/net/uk/rodgers/myrecipeapp/ApiService.kt
@@ -5,11 +5,11 @@ import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
 private val retrofit = Retrofit.Builder()
-    .baseUrl("www.themealdb.com/api/json/v1/1/categories.php")
+    .baseUrl("https://www.themealdb.com/api/json/v1/1/")
     .addConverterFactory(GsonConverterFactory.create())
     .build()
 
-val recipeService = retrofit.cre ate(ApiService::class.java)
+val recipeService = retrofit.create(ApiService::class.java)
 
 interface ApiService {
     @GET("categories.php")
